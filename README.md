a050cb67382a6ab0477e7a0c4b58c3cf131bd8d6
Zhang Hai <dreaming.in.code.zh@gmail.com>
2014-12-21 21:54:46 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,25 @@
+# Palette
+
+Sample application to showcase `android.support.v7.graphics.Palette`.
+
+Workarounds problems such as large bitmap, and uses [SystemBarTint](https://github.com/jgilfelt/SystemBarTint) library for system bar tinting.
+
+## Screenshot
+
+![Screenshot 1](screenshot/1.png)
+
+## License
+
+    Copyright 2014 Zhang Hai
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ app/src/main/java/me/zhanghai/palette/MainActivity.java
@@ -0,0 +1,198 @@
+/*
+ * Copyright (c) 2014 Zhang Hai <Dreaming.in.Code.ZH@Gmail.com>
+ * All Rights Reserved.
+ */
+
+package me.zhanghai.palette;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.graphics.Palette;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.readystatesoftware.systembartint.SystemBarTintManager;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends ActionBarActivity implements Palette.PaletteAsyncListener {
+
+    private static final int REQUEST_CODE_PICK_IMAGE = 0;
+
+    private ImageView image;
+    private View vibrantView;
+    private View vibrantLightView;
+    private View vibrantDarkView;
+    private View mutedView;
+    private View mutedLightView;
+    private View mutedDarkView;
+
+    private SystemBarTintManager systemBarTintManager;
+
+    private Bitmap bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        image = (ImageView)findViewById(R.id.image);
+        vibrantView = findViewById(R.id.vibrant);
+        vibrantLightView = findViewById(R.id.vibrant_light);
+        vibrantDarkView = findViewById(R.id.vibrant_dark);
+        mutedView = findViewById(R.id.muted);
+        mutedLightView = findViewById(R.id.muted_light);
+        mutedDarkView = findViewById(R.id.muted_dark);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            setTranslucentStatus(true);
+        }
+        systemBarTintManager = new SystemBarTintManager(this);
+        systemBarTintManager.setTintColor(getResources().getColor(resolveAttribute(
+                R.attr.colorPrimary)));
+        systemBarTintManager.setStatusBarTintEnabled(true);
+        systemBarTintManager.setNavigationBarTintEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_pick_image:
+                onPickImage();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_PICK_IMAGE) {
+            if (resultCode == RESULT_OK) {
+                onImagePicked(data.getData());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onGenerated(Palette palette) {
+        int transparent = getResources().getColor(android.R.color.transparent);
+        int vibrant = palette.getVibrantColor(transparent);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(vibrant));
+        systemBarTintManager.setTintColor(vibrant);
+        vibrantView.setBackgroundColor(vibrant);
+        vibrantLightView.setBackgroundColor(palette.getLightVibrantColor(transparent));
+        vibrantDarkView.setBackgroundColor(palette.getDarkVibrantColor(transparent));
+        mutedView.setBackgroundColor(palette.getMutedColor(transparent));
+        mutedLightView.setBackgroundColor(palette.getLightMutedColor(transparent));
+        mutedDarkView.setBackgroundColor(palette.getDarkMutedColor(transparent));
+    }
+
+    private void onPickImage() {
+
+        Intent pickImageIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        pickImageIntent.setType("image/*");
+
+        Intent chooserIntent = Intent.createChooser(pickImageIntent,
+                getString(R.string.action_pick_image));
+        startActivityForResult(chooserIntent, REQUEST_CODE_PICK_IMAGE);
+    }
+
+    @SuppressWarnings("deprecation")
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private void onImagePicked(Uri uri) {
+
+        if (bitmap != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                image.setBackground(null);
+            } else {
+                //noinspection deprecation
+                image.setBackgroundDrawable(null);
+            }
+            bitmap.recycle();
+        }
+
+        try {
+            bitmap = decodeBitmapUri(uri);
+            image.setImageBitmap(bitmap);
+            Palette.generateAsync(bitmap, this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @TargetApi(19)
+    private void setTranslucentStatus(boolean translucentStatus) {
+        Window window = getWindow();
+        WindowManager.LayoutParams attributes = window.getAttributes();
+        if (translucentStatus) {
+            attributes.flags |= WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        } else {
+            attributes.flags &= ~WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        }
+        window.setAttributes(attributes);
+    }
+
+    private int resolveAttribute(int attribute) {
+        TypedArray attributes = obtainStyledAttributes(new int[]{attribute});
+        int resId = attributes.getResourceId(0, -1);
+        attributes.recycle();
+        return resId;
+    }
+
+    private Bitmap decodeBitmapUri(Uri uri) throws IOException {
+
+        InputStream inputStream = getContentResolver().openInputStream(uri);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(inputStream, null, options);
+        inputStream.close();
+
+        inputStream = getContentResolver().openInputStream(uri);
+        // Canvas.getMaximumBitmapWidth() needs a hardware accelerated canvas to produce the right
+        // result, so we simply use 2048x2048 instead.
+        options.inSampleSize = computeInSampleSize(options, 2048, 2048);
+        options.inJustDecodeBounds = false;
+        Bitmap bitmap = BitmapFactory.decodeStream(inputStream, null, options);
+        inputStream.close();
+
+        return bitmap;
+    }
+
+    private int computeInSampleSize(BitmapFactory.Options options, int maxWidth, int maxHeight) {
+
+        int width = options.outWidth;
+        int height = options.outHeight;
+        int inSampleSize = 1;
+
+        while (width > maxWidth || height > maxHeight) {
+            inSampleSize *= 2;
+            width /= 2;
+            height /= 2;
+        }
+
+        return inSampleSize;
+    }
+}
